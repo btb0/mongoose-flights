716037c6bcf9c9019fce74f5db1abaee56c31a0e
btb0 <benburke2016@gmail.com>
2023-05-08 19:02:27 -0400

    displays default depart on /new

--- controllers/flights.js
+++ controllers/flights.js
@@ -15,7 +15,14 @@ async function index (req, res) {
 }
 
 function newFlight(req,res) {
-    res.render('flights/new', { errorMsg: '' });
+    const newFlight = new Flight();
+    // Obtain the default date
+    const dt = newFlight.departs;
+    // Format the date for the value attribute of the input
+    // yyyy-MM-ddThh:mm   ("T" is used to separate the date portion from the time portion)
+    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+    res.render('flights/new', { departsDate, errorMsg: '' }, console.log(departsDate));
 }
 
 async function create(req, res) {
