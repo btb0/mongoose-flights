a01a129087a91e34d7d5eb4b174fbbcb63667f1a
btb0 <benburke2016@gmail.com>
2023-05-08 02:33:55 -0400

    adds new/create functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,7 +2,9 @@
 var Flight = require('../models/flight');
 
 module.exports = {
-    index
+    index,
+    new: newFlight,
+    create
 }
 
 async function index (req, res) {
@@ -10,4 +12,21 @@ async function index (req, res) {
     res.render('flights/index', {
         flights: flights
     })
+}
+
+function newFlight(req,res) {
+    res.render('flights/new', { errorMsg: '' });
+}
+
+async function create(req, res) {
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key];
+    }
+    try {
+        await Flight.create(req.body)
+        res.redirect('/flights');
+    } catch {
+        console.log(err)
+        res.render('flights/new', { errorMsg: err.message });
+    }
 }
